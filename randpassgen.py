3eaf6120f88f06edb764f9039af8d64199650b2c
ParadymShift, Plant-BASED Ⓥegan Jedi <101068577+ParadymShift-Plant-BASED-Vegan-Jedi@users.noreply.github.com>
2023-02-07 20:17:36 -0700

    Done!
    It works quite nicely if I do say so myself :D

--- randpassgen.py
+++ randpassgen.py
@@ -1,3 +1,16 @@
 import random
 
-chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "~", "`", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/"]
+options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "~", "`", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/"]
+
+while True:
+    try:
+        chars = int(input("How long would you like your password to be? Enter number of characters here: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+passlist = []
+for i in range(chars):
+    passlist.append(random.choice(options))
+password = "".join(passlist)
+
+print(f"Your password is: \n\n{password}")
