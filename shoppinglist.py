0f5467c14abfba8658e4b5c4d4f74bb33edcaa98
ParadymShift, Plant-BASED Ⓥegan Jedi <101068577+ParadymShift-Plant-BASED-Vegan-Jedi@users.noreply.github.com>
2023-02-27 20:36:28 -0700

    Added the Suggestion Feature
    It's sorta working, but it definitely has at least one bug. Not sure why.

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,8 +1,10 @@
 thelist = []
+suggestions = {}
 print("""Welcome to the shopping list app. Here is a list of all of the commands:
 listview or lv: View shopping list
 listadd or la: Add item to shopping list
-itembought or ib: Mark item as bought (it will disappear from the list and can be added again the same way it was added before)""")
+listremove or lr: Mark item as bought (it will disappear from the list and can be added again the same way it was added before)
+suggest or suggestion: Suggest an item to add based on past history of items""")
 while True:
     userprompt = input("Enter Command Here: ")
     userprompt = userprompt.lower()
@@ -15,14 +17,48 @@ while True:
                 print("- " + li)
     elif userprompt == "listadd" or userprompt == "la":
         litem = input("Please enter your list item here: ")
+        litem = litem.strip()
         thelist.append(litem)
         print(f'Item "{litem}" added.')
-    elif userprompt == "itembought" or userprompt == "ib":
+    elif userprompt == "listremove" or userprompt == "lr":
         ritem = input("Please enter the bought item here: ")
+        ritem = ritem.strip()
         if ritem in thelist:
             thelist.remove(ritem)
             print(f'Item "{ritem}" removed.')
         else:
             print("Sorry, we can't seem to find that item. Check your spelling and capitalization and please try again.")
+        suggestions[ritem] = suggestions.get(ritem, 0) + 1
+    elif userprompt == "suggest" or userprompt == "suggestion":
+        if suggestions == {}:
+            print("There are currently no items which have been added and removed. To get suggestions, add some items and remove them! More popular items will be suggested first.")
+        else:
+            keys_sorted_by_count = [k for k, v in sorted(suggestions.items(), key=lambda item: item[1], reverse=True)]
+            print(keys_sorted_by_count[0])
+            index = 0
+            while True:
+                decision = input("Would you like another suggestion? Type y/n: ")
+                decision = decision.lower()
+                decision = decision.strip()
+                if decision == "yes" or decision == "y":
+                    print(keys_sorted_by_count[index + 1])
+                elif decision == "no" or decision == "n":
+                    break
+            """
+            largest_val = 0
+            best_suggestion = None
+            for suggestion, count in suggestions.items():
+                if count > largest_val:
+                    largest_val = count
+                    best_suggestion = suggestion
+            print(best_suggestion)
+            while True:
+                decision = input("Would you like another suggestion? Type y/n: ")
+                decision = decision.lower()
+                decision = decision.strip()
+                if decision == "yes" or decision == "y":
+                    sugglist = []
+                    for sugg, num in suggestions.items():
+            """
     else:
         print("Error. Command not recognized.")
