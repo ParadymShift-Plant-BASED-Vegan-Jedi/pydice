8ab826cce96e5b220afc6e4f1fb63544d82ee268
ParadymShift, Plant-BASED Ⓥegan Jedi <101068577+ParadymShift-Plant-BASED-Vegan-Jedi@users.noreply.github.com>
2023-02-28 19:37:40 -0700

    Most Stable Version
    I tried to include a feature where it wont suggest items that are already in your list, but it proved too troublesome at the moment.

--- ShoppingListApp.py
+++ ShoppingListApp.py
@@ -72,6 +72,22 @@ viewcommands or vc: Show this list of commands""",
             self.intro.text = "Sorry, we could not find that item in your list! \nPlease check spelling/capitalization and try again"
             self.button.unbind(on_press=self.removeitem)
             self.button.bind(on_press=self.callback)
+    
+    def suggest(self, instance):
+        self.userinput.text = self.userinput.text.lower()
+        self.userinput.text = self.userinput.text.strip()
+        if self.userinput.text == "yes" or self.userinput.text == "y":
+            try:
+                self.intro.text = f"{self.keys_sorted_by_count[self.index + 1]}\nWould you like another suggestion? Type y/n:"
+                self.index = self.index + 1
+            except:
+                self.intro.text = "No more suggestions."
+                self.button.unbind(on_press=self.suggest)
+                self.button.bind(on_press=self.callback)
+        elif self.userinput.text == "no" or self.userinput.text == "n":
+            self.intro.text = "Okay!"
+            self.button.unbind(on_press=self.suggest)
+            self.button.bind(on_press=self.callback)
 
     def callback(self, instance):
         self.userinput.text = self.userinput.text.lower()
@@ -85,10 +101,34 @@ viewcommands or vc: Show this list of commands""",
                     self.intro.text += f"- {item}\n"
         elif self.userinput.text == "la" or self.userinput.text == "listadd":
             self.intro.text = "Type the item you would like to add."
+            self.button.unbind(on_press=self.callback)
             self.button.bind(on_press=self.additem)
         elif self.userinput.text == "lr" or self.userinput.text == "listremove":
             self.intro.text = "Type the item you would like to remove."
+            self.button.unbind(on_press=self.callback)
             self.button.bind(on_press=self.removeitem)
+        elif self.userinput.text == "vc" or self.userinput.text == "viewcommands":
+            self.intro.text = """Welcome to the shopping list app.
+Here is a list of all of the commands:
+listview or lv: View shopping list
+listadd or la: Add item to shopping list
+listremove or lr: Mark item as bought 
+(it will disappear from the list and can 
+be added again the same way it was added before)
+suggest or suggestion: Suggest an item to add 
+based on past history of items
+viewcommands or vc: Show this list of commands"""
+        elif self.userinput.text == "suggest" or self.userinput.text == "suggestion":
+            if self.suggestions == {}:
+                self.intro.text = "There are currently no items which have been added and removed.\nTo get suggestions, add some items and remove them!\nMore popular items will be suggested first."
+            else:
+                self.index = 0
+                self.keys_sorted_by_count = [k for k, v in sorted(self.suggestions.items(), key=lambda item: item[1], reverse=True)]
+                self.intro.text = f"{self.keys_sorted_by_count[0]}\nWould you like another suggestion? Type y/n:"
+                self.button.unbind(on_press=self.callback)
+                self.button.bind(on_press=self.suggest)
+        else:
+            self.intro.text = "Error. Command not recognized."
             
 
 
